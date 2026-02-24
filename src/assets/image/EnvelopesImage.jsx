7f3f0c59a8b1f1045bfff516f3f7eb2edc3cdca0
nicole <nicole0322n@gmail.com>
2026-02-24 19:23:58 +0800

    feat: 區塊設定 完成信封區塊

---
+++ src/assets/image/EnvelopesImage.jsx
@@ -0,0 +1,120 @@
+export default function EnvelopesImage(props) {
+  return (
+    <svg
+      width="1488"
+      height="639"
+      viewBox="0 0 1488 639"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g filter="url(#filter0_d_42_216)">
+        <rect x="13" width="1449" height="631" fill="#454837" />
+      </g>
+      <path
+        d="M709.883 180.994C726.391 170.338 747.61 170.338 764.117 180.994L1462 631.489L12 631.489L709.883 180.994Z"
+        fill="url(#paint0_linear_42_216)"
+      />
+      <g filter="url(#filter1_d_42_216)">
+        <path
+          d="M764.103 449.519C747.602 460.162 726.398 460.162 709.897 449.519L13 0H1461L764.103 449.519Z"
+          fill="url(#paint1_linear_42_216)"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_42_216"
+          x="9"
+          y="0"
+          width="1457"
+          height="639"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_42_216"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_42_216"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter1_d_42_216"
+          x="0"
+          y="0"
+          width="1488"
+          height="502.501"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dx="7" dy="25" />
+          <feGaussianBlur stdDeviation="10" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.6 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_42_216"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_42_216"
+            result="shape"
+          />
+        </filter>
+        <linearGradient
+          id="paint0_linear_42_216"
+          x1="737"
+          y1="244"
+          x2="737"
+          y2="631.489"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.456731" stop-color="#838967" />
+          <stop offset="1" stop-color="#5D604D" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_42_216"
+          x1="737"
+          y1="121"
+          x2="737"
+          y2="457.489"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#7B8062" />
+          <stop offset="1" stop-color="#7B8062" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
--- src/components/Envelopes.jsx
+++ src/components/Envelopes.jsx
@@ -1,5 +1,35 @@
+import EnvelopesImage from "../assets/image/EnvelopesImage.jsx";
+
 function Envelopes() {
-  return <>Envelopes</>;
+  return (
+    <div className="">
+      <div className="d-flex justify-content-center overflow-hidden position-relative">
+        <div style={{ width: "1488px" }}>
+          <EnvelopesImage />
+        </div>
+
+        <h1
+          className="position-absolute  start-50 translate-middle text-white text-center italianno-regular"
+          style={{ fontSize: 54, top: "40%" }}
+        >
+          Welcome
+          <br />
+          To
+          <br />
+          Our Wedding
+        </h1>
+
+        <span
+          className="position-absolute start-50 translate-middle text-white text-center"
+          style={{ bottom: "3%" }}
+        >
+          廖慶武＆黃婉暄
+          <br />
+          2026.03.21
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Envelopes;
