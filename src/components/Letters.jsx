b8780ba3e73520ddb3d500718e9015e0609a2479
nicole <nicole0322n@gmail.com>
2026-02-24 23:07:47 +0800

    feat: 區塊設置 完成喜帖區

--- src/components/Letters.jsx
+++ src/components/Letters.jsx
@@ -1,5 +1,13 @@
 function Letters() {
-  return <>Letters</>;
+  return (
+    <div className="mx-3 my-5">
+      <img
+        src="./image/LettersImage.jpg"
+        className="d-block w-100"
+        alt="LettersImage"
+      />
+    </div>
+  );
 }
 
 export default Letters;
