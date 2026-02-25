f4f6b900ce118f2d4d1001dae990a00c11ea2cd5
nicole <nicole0322n@gmail.com>
2026-02-25 13:47:13 +0800

    feat: 區塊設置 完成喜帖區

--- src/components/Letters.jsx
+++ src/components/Letters.jsx
@@ -1,11 +1,34 @@
 function Letters() {
   return (
-    <div className="mx-3 my-5">
+    <div className="card mx-3 my-5 bg-secondary" style={{ borderRadius: 0 }}>
       <img
-        src="./image/LettersImage.jpg"
-        className="d-block w-100"
+        src="./image/LetterImage.svg"
+        className="card-img"
         alt="LettersImage"
       />
+      <div className="card-img-overlay text-center">
+        <img
+          src="./image/Letter-title.svg"
+          className="my-2"
+          style={{ maxWidth: "56%" }}
+          alt="LettersTitle"
+        />
+        <img
+          src="./image/Letter-people.svg"
+          className="card-words my-2"
+          alt="LettersPeople"
+        />
+        <img
+          src="./image/Letter-date.svg"
+          className="card-words my-4"
+          alt="LettersDate"
+        />
+        <img
+          src="./image/Letter-map.svg"
+          className="card-words mt-2"
+          alt="LettersMap"
+        />
+      </div>
     </div>
   );
 }
